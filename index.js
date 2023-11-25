96d1159e7cc011d6311a40db54699fcb2104e407
Mehedi Hassan <mh93075@gmail.com>
2023-11-25 20:16:50 +0600

    Post method added for survey creation

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -22,6 +22,43 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+    const surveyCollection = client.db('surveyDB').collection('survey');
+
+    app.get('/survey', async(req, res)=>{
+        const cursor = surveyCollection.find();
+        // console.log('tok tok token', req.cookies.token)
+        const result = await cursor.toArray();
+        res.send(result);
+        console.log(result)
+      })
+      
+      app.get('/survey/:_id', async (req, res) => {
+        try {
+          const id = req.params._id;
+          const query = { _id: new ObjectId(id) };
+          const survey = await surveyCollection.findOne(query);
+      
+          if (!survey) {
+            return res.status(404).send('Survey not found');
+          }
+      
+          res.send(survey);
+        } catch (error) {
+          console.error('Error fetching survey by ID:', error);
+          res.status(500).send('Internal Server Error');
+        }
+      });
+      app.post('/survey', async(req, res)=>{
+        const job = req.body;
+        // console.log(job);
+        const result = await surveyCollection.insertOne(job)
+        res.send(result)
+        
+    })
+
+
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
