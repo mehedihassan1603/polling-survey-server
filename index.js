e07ee9d5690bb491905c4e7275d0a61b7a7a6a48
Mehedi Hassan <mh93075@gmail.com>
2023-11-27 07:19:10 +0600

    Verify token use middlewares and apply

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const app = express();
@@ -32,6 +33,33 @@ async function run() {
 
     const surveyCollection = client.db('surveyDB').collection('survey');
     const userCollection = client.db('surveyDB').collection('users');
+
+
+    // jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    })
+
+    // middlewares 
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+
+
 
     app.get('/survey', async(req, res)=>{
         const cursor = surveyCollection.find();
@@ -79,7 +107,8 @@ async function run() {
       res.send(result)
   })
 
-  app.get('/users', async (req, res) => {
+  app.get('/users', verifyToken, async (req, res) => {
+    // console.log(req.headers)
     const result = await userCollection.find().toArray();
     res.send(result);
   });
